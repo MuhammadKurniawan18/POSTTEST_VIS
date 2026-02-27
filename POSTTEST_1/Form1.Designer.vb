e29887941fb8ad4949e870f8befb5f114c1f6747
MuhammadKurniawan18 <muhkurniawan002@gmail.com>
2026-02-27 22:50:43 +0800

    Posttest 1

---
+++ POSTTEST_1/Form1.Designer.vb
@@ -0,0 +1,119 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lblsmt = New Label()
+        txtSmt = New TextBox()
+        txtIPK = New TextBox()
+        lblipk = New Label()
+        btntambah = New Button()
+        btnreset = New Button()
+        lblKategori = New Label()
+        SuspendLayout()
+        ' 
+        ' lblsmt
+        ' 
+        lblsmt.AutoSize = True
+        lblsmt.Location = New Point(207, 119)
+        lblsmt.Name = "lblsmt"
+        lblsmt.Size = New Size(115, 25)
+        lblsmt.TabIndex = 0
+        lblsmt.Text = "IP SEMESTER"
+        ' 
+        ' txtSmt
+        ' 
+        txtSmt.Location = New Point(371, 116)
+        txtSmt.Name = "txtSmt"
+        txtSmt.Size = New Size(216, 31)
+        txtSmt.TabIndex = 1
+        ' 
+        ' txtIPK
+        ' 
+        txtIPK.Location = New Point(371, 212)
+        txtIPK.Name = "txtIPK"
+        txtIPK.Size = New Size(216, 31)
+        txtIPK.TabIndex = 3
+        ' 
+        ' lblipk
+        ' 
+        lblipk.AutoSize = True
+        lblipk.Location = New Point(207, 215)
+        lblipk.Name = "lblipk"
+        lblipk.Size = New Size(125, 25)
+        lblipk.TabIndex = 2
+        lblipk.Text = "IP KUMULATIF"
+        ' 
+        ' btntambah
+        ' 
+        btntambah.Location = New Point(371, 162)
+        btntambah.Name = "btntambah"
+        btntambah.Size = New Size(98, 34)
+        btntambah.TabIndex = 4
+        btntambah.Text = "Tambah"
+        btntambah.UseVisualStyleBackColor = True
+        ' 
+        ' btnreset
+        ' 
+        btnreset.Location = New Point(489, 162)
+        btnreset.Name = "btnreset"
+        btnreset.Size = New Size(98, 34)
+        btnreset.TabIndex = 5
+        btnreset.Text = "Reset"
+        btnreset.UseVisualStyleBackColor = True
+        ' 
+        ' lblKategori
+        ' 
+        lblKategori.AutoSize = True
+        lblKategori.Location = New Point(335, 281)
+        lblKategori.Name = "lblKategori"
+        lblKategori.Size = New Size(77, 25)
+        lblKategori.TabIndex = 6
+        lblKategori.Text = "kategori"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(lblKategori)
+        Controls.Add(btnreset)
+        Controls.Add(btntambah)
+        Controls.Add(txtIPK)
+        Controls.Add(lblipk)
+        Controls.Add(txtSmt)
+        Controls.Add(lblsmt)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblsmt As Label
+    Friend WithEvents txtSmt As TextBox
+    Friend WithEvents txtIPK As TextBox
+    Friend WithEvents lblipk As Label
+    Friend WithEvents btntambah As Button
+    Friend WithEvents btnreset As Button
+    Friend WithEvents lblKategori As Label
+
+End Class
---
+++ POSTTEST_1/Form1.vb
@@ -0,0 +1,60 @@
+﻿Public Class Form1
+    Dim IPK As Double = 0
+    Dim IPS As Double = 0
+    Dim semester As Integer = 0
+    Dim TotalIP As Double = 0
+
+    Private Sub ValidateInput()
+        If Double.TryParse(txtSmt.Text, IPS) Then
+            ' angka berhasil dibaca
+        Else
+            MessageBox.Show("Input Harus Angka!")
+        End If
+    End Sub
+
+    Private Sub kategoriIPK()
+        If IPK >= 2.0 AndAlso IPK <= 2.75 Then
+            lblKategori.Text = "Cukup"
+        ElseIf IPK >= 2.76 AndAlso IPK <= 3.0 Then
+            lblKategori.Text = "Memuaskan"
+        ElseIf IPK >= 3.01 Then
+            lblKategori.Text = "Sangat Memuaskan"
+        Else
+            lblKategori.Text = "kategori"
+        End If
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles lblsmt.Click
+
+    End Sub
+
+    Private Sub Label1_Click_1(sender As Object, e As EventArgs) Handles lblKategori.Click
+
+    End Sub
+
+    Private Sub txtsmt_TextChanged(sender As Object, e As EventArgs) Handles txtSmt.TextChanged
+
+    End Sub
+
+    Private Sub txtipk_TextChanged(sender As Object, e As EventArgs) Handles txtIPK.TextChanged
+
+    End Sub
+
+    Private Sub btntambah_Click(sender As Object, e As EventArgs) Handles btntambah.Click
+        ValidateInput()
+        semester += 1
+        TotalIP += IPS
+        IPK = TotalIP / semester
+        txtIPK.Text = IPK.ToString("0.00")
+        KategoriIPK()
+        txtSmt.Clear()
+    End Sub
+
+    Private Sub btnreset_Click(sender As Object, e As EventArgs) Handles btnreset.Click
+        semester = 0
+        TotalIP = 0
+        txtSmt.Clear()
+        txtIPK.Clear()
+        lblKategori.Text = "kategori"
+    End Sub
+End Class
